fd553970aa771c86d41ddbe73c0ff7830aa75eaf
Matheus Mombach <mombachm@gmail.com>
2019-09-10 14:55:00 -0300

    start Node Express base application

---
+++ src/App.ts
@@ -0,0 +1,21 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { MainController } from "./routingControllers/MainController";
+
+class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config(): void {
+    // support application/json
+    this.app.use(bodyParser.json());
+    //support application/x-www-form-urlencoded post data
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use("/", new MainController().getRouter());
+  }
+}
+export default new App().app;
