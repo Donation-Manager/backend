ca5f48d2246386e62c7b075ead07f2d7f7be5f41
Matheus Mombach <matheus.mombach@sap.com>
2019-09-11 16:38:21 -0300

    add tslint dependency

--- src/models/Donation.ts
+++ src/models/Donation.ts
@@ -4,7 +4,7 @@ import { ModelName } from "./definitions/Model";
 import { assembleModel } from "./definitions/ModelAssembler";
 
 export interface DonationModel extends Document {
-  description: string
+  description: string;
 }
 
 const DonationSchema = new Schema({
