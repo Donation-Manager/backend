130447d42a63ac2e79223f5109fb93938e7db8a7
Matheus Mombach <mombachm@gmail.com>
2019-09-10 22:29:58 -0300

    add call to remove all app listeners to work better with nodemon

--- src/Server.ts
+++ src/Server.ts
@@ -2,6 +2,7 @@ import app from "./App";
 import { MongoDBConnection } from "./database/MongoDBConnection";
 const PORT = 3000;
 
+app.removeAllListeners();
 app.listen(PORT, () => {
   console.log("Express server listening on port " + PORT);
   MongoDBConnection.getInstance().connect();
