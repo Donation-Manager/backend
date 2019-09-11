f28bc9032667f149b16322016411c281f5defb5f
Matheus Mombach <mombachm@gmail.com>
2019-09-10 21:06:32 -0300

    create a connection with MongoDB via Mongoose

--- src/Server.ts
+++ src/Server.ts
@@ -1,6 +1,8 @@
 import app from "./App";
+import { MongoDBConnection } from "./database/MongoDBConnection";
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log("Express server listening on port " + PORT);
+  MongoDBConnection.getInstance().connect();
 });
