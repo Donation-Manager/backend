1247f360141cadd170a540d34741edba03fa92cf
Leonardo Broch de Morais <leonardo.morais@cwi.com.br>
2019-11-19 22:31:04 -0300

    Vincular intenção de doação a uma necessidade

--- src/models/DonationIntention.ts
+++ src/models/DonationIntention.ts
@@ -3,19 +3,22 @@ import { Schema, Document } from "mongoose";
 import { ModelName } from "./definitions/Model";
 import { assembleModel } from "./definitions/ModelAssembler";
 import { GiverModel } from "./Giver";
+import { DonationNeedModel } from "./DonationNeed";
 
 export interface DonationIntentionModel extends Document {
   collectFromGiver: boolean;
   collectDate: Date;
   description: string;
   giver: GiverModel;
+  donationNeed: DonationNeedModel;
 }
 
 const DonationIntentionSchema = new Schema({
   collectFromGiver: { type: Boolean },
   collectDate: { type: Date },
   description: { type: String },
-  giver: { type: Schema.Types.ObjectId, ref: ModelName.Giver }
+  giver: { type: Schema.Types.ObjectId, ref: ModelName.Giver },
+  donationNeed: { type: Schema.Types.ObjectId, ref: ModelName.DonationNeed }
 });
 
 export default assembleModel<DonationIntentionModel>(ModelName.DonationIntention, DonationIntentionSchema);
